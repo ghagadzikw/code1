d4bd6f18702cf5c9d44ea3404708106e134ae2fc
Kwame <agamedium1@gmail.com>
2018-12-09 17:39:19 +0000

    first commit

---
+++ console.py
@@ -0,0 +1,21 @@
+from box import Case, Carton
+from tkinter import *
+
+root = Tk()
+
+label = Label()
+label.pack()
+
+milo = Case()
+
+entry = Entry()
+entry.pack()
+
+def setValue():
+	label["text"] = milo.calculateBoxCompression(int(Entry.get(entry)),300,400)
+
+button = Button(text = "calculate",command = setValue)
+button.pack()
+
+
+root.mainloop()
